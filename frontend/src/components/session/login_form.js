e3a847deda74d2d314b586e9b35706125d23b267
kxwzhang <kxwzhang@gmail.com>
2020-08-31 10:54:27 -0700

    Add className for login form

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -58,7 +58,7 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='login-form-container'>
         <form onSubmit={this.handleSubmit}>
           <div>
             <input
