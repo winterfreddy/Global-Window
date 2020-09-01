2b154aeb775f9365dc0eaecfcfad692f3d8d32e9
Alex Choy <alexchoy2345@gmail.com>
2020-09-01 14:26:29 -0700

    finalized posting photos to /api/photos/

--- routes/api/photos.js
+++ routes/api/photos.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const passport = require("passport");
 const AWS = require('aws-sdk');
 const uuidv4 = require("uuid").v4;
-const fs = require('fs');
 const keys = require("../../config/keys");
 const Photo = require("../../models/Photo");
 const validatePhotoInput = require("../../validation/photo");
@@ -12,18 +11,13 @@ const validatePhotoInput = require("../../validation/photo");
 const multer = require("multer");
 const upload = multer();
 
-// AWS Setup
+// AWS S3 Setup
 const s3 = new AWS.S3({
   accessKeyId: keys.awsAccessKeyId,
   secretAccessKey: keys.awsSecretAccessKey,
 });
-const config = new AWS.Config({
-  accessKeyId: keys.awsAccessKeyId,
-  secretAccessKey: keys.awsSecretAccessKey,
-  region: "us-west-1",
-});
 
-const uploadFile = (file) => {
+const uploadImage = (file) => {
   const params = {
     Bucket: keys.s3Bucket,
     Key: uuidv4(),
@@ -47,22 +41,21 @@ router.post("/", upload.single("file"),
         }
         
         // Wait for file to upload, get URL, and then create Photo object
-        const uploadedFileURL = uploadFile(req.file)
-            .then(data => {
-              const uploadedFileURL = data.Location;
+        uploadImage(req.file).then(data => {
+            const uploadedFileURL = data.Location;
 
             // create Photo object
-              const newPhoto = new Photo({
-                creatorId: req.user.id, // req.user.id
-                imageURL: uploadedFileURL,
-                coordinates: { lat: req.body.lat, lng: req.body.lng },
-              });
-
-              newPhoto.save().then((photo) => res.json(photo));
-            }).catch(err => {
-                console.log(err);  
-                res.status(400).json({image: "Image upload did not work"})
+            const newPhoto = new Photo({
+            creatorId: req.user.id, // req.user.id
+            imageURL: uploadedFileURL,
+            coordinates: { lat: req.body.lat, lng: req.body.lng },
             });
+
+            newPhoto.save().then((photo) => res.json(photo));
+        }).catch(err => {
+            console.log(err);  
+            res.status(400).json({image: "Image upload did not work"});
+        });
     }
 );
 
