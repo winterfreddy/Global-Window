3a01b095d30e54ed94e06c7711bc8035563ef934
Alex Choy <alexchoy2345@gmail.com>
2020-09-01 12:24:10 -0700

    form-data from postman shows up except for the image

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
+  s3Bucket: process.env.GLOBAL_WINDOW_PROJECT_PROD,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 };
+
--- routes/api/photos.js
+++ routes/api/photos.js
@@ -2,35 +2,37 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const uuidv4 = require("uuid").v4;
+const fs = require('fs');
 const keys = require("../../config/keys");
+const Photo = require("../../models/Photo");
+const validatePhotoInput = require("../../validation/photo");
+
+// Middleware for Form-Data Postman
+const multer = require("multer");
+const upload = multer();
 
 // AWS Setup
+const s3 = new AWS.S3({
+  accessKeyId: keys.awsAccessKeyId,
+  secretAccessKey: keys.awsSecretAccessKey,
+});
 const config = new AWS.Config({
-    accessKeyId: keys.aws_access_key_id,
-    secretAccessKey: keys.aws_secret_access_key,
-    region: "us-west-1"
+  accessKeyId: keys.awsAccessKeyId,
+  secretAccessKey: keys.awsSecretAccessKey,
+  region: "us-west-1",
 });
-// var config = new AWS.Config({
-//   accessKeyId: 'AKID', secretAccessKey: 'SECRET', region: 'us-west-2'
+
+// router.get("/", (req, res) => {
+//     config.getCredentials(function (err) {
+//         if (err) console.log(err.stack);
+//         // credentials not loaded
+//         else {
+//             console.log("Access key:", config.credentials.accessKeyId);
+//         }
+//     });
+//     // res.json("Finished?");
 // });
-// AWS.config.accessKeyId = keys.aws_access_key_id;
-// AWS.config.secreAccessKey = keys.aws_secret_access_key;
-// AWS.config.region = "us-west-1";
-
-
-
-
-router.get("/", (req, res) => {
-    console.log("Get Photos");
-    config.getCredentials(function (err) {
-        if (err) console.log(err.stack);
-        // credentials not loaded
-        else {
-            console.log("Access key:", config.credentials.accessKeyId);
-        }
-    });
-});
 
 // Use this to access/add files?
 // var params = { Bucket: "myBucket", Key: "fileName" }
@@ -40,21 +42,48 @@ router.get("/", (req, res) => {
 //   else     console.log(data);
 // });
 
-const Photo = require("../../models/Photo");
-const validatePhotoInput = require("../../validation/photo");
 
-router.post("/", 
+const uploadFile = (file) => {
+  // Read content from the file
+  console.log(file);
+  const fileContent = fs.readFileSync(file);
+
+  const newFileName = uuidv4();
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: keys.s3Bucket,
+    Key: newFileName, // File name you want to save as in S3
+    Body: fileContent,
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+    // uploadedFileURL = data.Location;
+  });
+  return data.Location;
+};
+
+router.post("/", upload.single("file"), 
     passport.authenticate("jwt", { session: false }),
     (req, res) => {
-        const { errors, isValid } = validatePhotoInput(req.body);
 
-        if (!isValid) {
-            return res.status(400).json(errors);
-        }
+        console.log('req: ', req);
+        console.log('body: ', req.body);
+        // const { errors, isValid } = validatePhotoInput(req.body);
+
+        // if (!isValid) {
+        //     return res.status(400).json(errors);
+        // }
+
+        const uploadedFileURL = uploadFile(req.body.file);
     
         const newPhoto = new Photo({
-            creatorId: req.body.creatorId,
-            imageURL: req.body.imageURL,
+            creatorId: req.body.creatorId, // req.user.id
+            imageURL: uploadedFileURL,
             coordinates: { lat: req.body.lat, lng: req.body.lng }
         });
 
