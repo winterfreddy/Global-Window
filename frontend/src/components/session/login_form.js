2598427d8f0f748afc3a9d18bfe4b7cc71f475e9
kxwzhang <kxwzhang@gmail.com>
2020-08-31 17:53:02 -0700

    Tag on a second promise for closeModal

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -44,10 +44,6 @@ class LoginForm extends React.Component {
 
     this.props.login(user)
       .then(this.props.history.push('/home'))
- 
-
-
-
   }
 
   // Render the session errors if there are any
