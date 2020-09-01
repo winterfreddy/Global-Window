b01c8f2dc70354bfed58a057a47be6b38453287c
kxwzhang <kxwzhang@gmail.com>
2020-08-31 18:35:43 -0700

    Add functions for pinning and removing

--- frontend/src/components/maps/google_maps_container.jsx
+++ frontend/src/components/maps/google_maps_container.jsx
@@ -2,35 +2,51 @@ import React, { Component } from "react";
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
 const mapStyles = {
-  width: "100%",
-  height: "100%",
+  width: "50%",
+  height: "50%",
 };
 
 export class MapContainer extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-          showingInfoWindow: false, //Hides or the shows the infoWindow
-          activeMarker: {}, //Shows the active marker upon click
-          selectedPlace: {}, //Shows the infoWindow to the selected place upon a marker
-        };
-    }
+    this.state = {
+      showingInfoWindow: false, //Hides or the shows the infoWindow
+      activeMarker: {}, //Shows the active marker upon click
+      selectedPlace: {}, //Shows the infoWindow to the selected place upon a marker
+    };
+  }
 
-    render() {
-        return (
-        <Map
-            className='google-api-map'
-            google={this.props.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={{
-            lat: -1.2884,
-            lng: 36.8233,
-            }}
-        />
-        );
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
     }
+  };
+
+  render() {
+    return (
+      <Map
+        className="google-api-map"
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: -1.2884,
+          lng: 36.8233,
+        }}
+      />
+    );
+  }
 }
 
 export default GoogleApiWrapper({
