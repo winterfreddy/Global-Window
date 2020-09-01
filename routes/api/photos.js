fd7ff40db2b30f84ae832b6d161c1a07b7cf4621
Alex Choy <alexchoy2345@gmail.com>
2020-09-01 14:22:42 -0700

    file uploads to S3, no more hanging promise

--- routes/api/photos.js
+++ routes/api/photos.js
@@ -23,86 +23,46 @@ const config = new AWS.Config({
   region: "us-west-1",
 });
 
-// router.get("/", (req, res) => {
-//     config.getCredentials(function (err) {
-//         if (err) console.log(err.stack);
-//         // credentials not loaded
-//         else {
-//             console.log("Access key:", config.credentials.accessKeyId);
-//         }
-//     });
-//     // res.json("Finished?");
-// });
-
-// Use this to access/add files?
-// var params = { Bucket: "myBucket", Key: "fileName" }
-
-// s3.getObject(params, function(err, data) {
-//   if (err) console.log(err, err.stack);
-//   else     console.log(data);
-// });
-
-
 const uploadFile = (file) => {
-  // Read content from the file
-  console.log(file);
-//   const fileContent = fs.readFileSync(file.buffer);
-//   const fileContent = fs.readFile(file, 'utf8');
-    // const fileContent = fs.readFile(file,(err, data) => {
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
-
-//   const newFileName = uuidv4();
-  // Setting up S3 upload parameters
   const params = {
     Bucket: keys.s3Bucket,
-    Key: file.originalname, // File name you want to save as in S3
+    Key: uuidv4(),
     Body: file.buffer,
     ContentType: file.mimetype,
     ACL: "public-read"
   };
 
-  let uploadedFileURL;
-  // Uploading files to the bucket
-
-    // if (err) {
-    //   throw err;
-    // }
-    // console.log(`File uploaded successfully. ${data.Location}`);
-    // uploadedFileURL = data.Location;
   const uploadPhoto = s3.upload(params).promise();
   
-  return uploadPhoto.then((data) => {
-    console.log(data.Location);
-    return data.Location;  
-  }).catch((err) => {
-      console.log('photo error: ', err);
-  });
+  return s3.upload(params).promise();
 };
 
 router.post("/", upload.single("file"), 
     passport.authenticate("jwt", { session: false }),
     (req, res) => {
+        const { errors, isValid } = validatePhotoInput(req.body);
 
-        // console.log('req: ', req);
-        // console.log('body: ', req.body);
-        // console.log('file: ', req.file);
-        // const { errors, isValid } = validatePhotoInput(req.body);
+        if (!isValid) {
+          return res.status(400).json(errors);
+        }
+        
+        // Wait for file to upload, get URL, and then create Photo object
+        const uploadedFileURL = uploadFile(req.file)
+            .then(data => {
+              const uploadedFileURL = data.Location;
 
-        // if (!isValid) {
-        //     return res.status(400).json(errors);
-        // }
+            // create Photo object
+              const newPhoto = new Photo({
+                creatorId: req.user.id, // req.user.id
+                imageURL: uploadedFileURL,
+                coordinates: { lat: req.body.lat, lng: req.body.lng },
+              });
 
-        const uploadedFileURL = uploadFile(req.file);
-        console.log("received uploadedFileURL");
-        const newPhoto = new Photo({
-            creatorId: req.body.creatorId, // req.user.id
-            imageURL: uploadedFileURL,
-            coordinates: { lat: req.body.lat, lng: req.body.lng }
-        });
-
-        newPhoto.save().then((photo) => res.json(photo));
+              newPhoto.save().then((photo) => res.json(photo));
+            }).catch(err => {
+                console.log(err);  
+                res.status(400).json({image: "Image upload did not work"})
+            });
     }
 );
 
