5312f578e22de33c357176bd6278251cc1eabed5
kxwzhang <kxwzhang@gmail.com>
2020-08-31 18:20:02 -0700

    Create splash jsx

---
+++ frontend/src/components/splash/splash.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Splash extends React.Component {
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+
+    render() {
+        return (
+            <div className='splash-container'>
+
+            </div>
+        );
+    }
+}
