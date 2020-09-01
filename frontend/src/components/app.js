05e9c7da9988059819a33997fb41ee98b92d4af8
kxwzhang <kxwzhang@gmail.com>
2020-08-31 18:09:27 -0700

    Add in GoogleMapsContainer to app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,12 +4,13 @@ import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 import MainPage from "./main/main_page";
 import Modal from './modal/modal';
-import GoogleMaps from './maps/google_maps_container';
+import GoogleMapsContainer from './maps/google_maps_container';
 
 const App = () => (
   <div>
     <Modal />
     <NavBarContainer />
+    <GoogleMapsContainer />
     <Switch>
       <AuthRoute exact path="/home" component={MainPage} />
     </Switch>
