fec6dadb2b6fce78f684bd11246dc69edfba931d
kxwzhang <kxwzhang@gmail.com>
2020-09-01 21:46:10 -0700

    Add headers

--- frontend/src/components/maps/image_upload_form.jsx
+++ frontend/src/components/maps/image_upload_form.jsx
@@ -42,11 +42,16 @@ class ImageUploadForm extends React.Component {
     const formData = new FormData();
     if (this.state.photoFile) {
       // formData.append('description', this.state.description);
-      formData.append('imageURL', this.state.photoFile);
+      // formData.append('imageURL', this.state.photoFile);
       let coordinates = { lat: this.state.lat, lng: this.state.lng};
       formData.append('coordinates', coordinates);
 
-      axios.post('/api/photos/',formData)
+      axios.post('/api/photos/', formData, {
+          headers: {
+            accept: "application/json",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+          }})
         .then( (response) => {
           if (response.status === 200) {
             if (response.data.error) {
@@ -59,38 +64,38 @@ class ImageUploadForm extends React.Component {
         })
     }
 
-    // If file selected
+    // // If file selected
     // if (this.state.photoFile) {
-      // data.append(
-      //   "description",
-      //   this.state.lat,
-      //   this.state.lng,
-      //   this.state.photoFile,
-      //   this.state.photoFile.name
-      // );
-      // axios
-      //   .post("/api/photos/", data, {
-      //     headers: {
-      //       accept: "application/json",
-      //       "Accept-Language": "en-US,en;q=0.8",
-      //       "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
-      //     },
-      //   })
-      //   .then((response) => {
-      //     if (200 === response.status) {
-      //       // If file size is larger than expected.
-      //       if (response.data.error) {
-      //         if ("LIMIT_FILE_SIZE" === response.data.error.code) {
-      //         } else {
-      //           console.log(response.data);
-      //         }
-      //       } else {
-      //         // Success
-      //         let fileName = response.data;
-      //         console.log("fileName", fileName);
-      //       }
-      //     }
-      //   });
+    //   data.append(
+    //     "description",
+    //     this.state.lat,
+    //     this.state.lng,
+    //     this.state.photoFile,
+    //     this.state.photoFile.name
+    //   );
+    //   axios
+    //     .post("/api/photos/", data, {
+    //       headers: {
+    //         accept: "application/json",
+    //         "Accept-Language": "en-US,en;q=0.8",
+    //         "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+    //       },
+    //     })
+    //     .then((response) => {
+    //       if (200 === response.status) {
+    //         // If file size is larger than expected.
+    //         if (response.data.error) {
+    //           if ("LIMIT_FILE_SIZE" === response.data.error.code) {
+    //           } else {
+    //             console.log(response.data);
+    //           }
+    //         } else {
+    //           // Success
+    //           let fileName = response.data;
+    //           console.log("fileName", fileName);
+    //         }
+    //       }
+    //     });
     // } 
   };
 
