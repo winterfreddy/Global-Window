8f69572657400224743e023cb6102d118c3cd317
kxwzhang <kxwzhang@gmail.com>
2020-09-01 20:29:07 -0700

    Add _onDrop

--- frontend/src/components/maps/image_upload_form.jsx
+++ frontend/src/components/maps/image_upload_form.jsx
@@ -43,12 +43,14 @@ class ImageUploadForm extends React.Component {
     // If file selected
     if (this.state.selectedFile) {
       data.append(
-        "profileImage",
+        "description",
+        this.state.lat,
+        this.state.lng,
         this.state.selectedFile,
         this.state.selectedFile.name
       );
       axios
-        .post("/api/profile/profile-img-upload", data, {
+        .post("/api/photos/", data, {
           headers: {
             accept: "application/json",
             "Accept-Language": "en-US,en;q=0.8",
