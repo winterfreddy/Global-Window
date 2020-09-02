61cc99f13eaa46f5de51298ab3d0ca4f14e90dd2
kxwzhang <kxwzhang@gmail.com>
2020-09-02 16:09:02 -0700

    Style hover on sidebar

--- frontend/src/components/sidebar/sidebar_item.jsx
+++ frontend/src/components/sidebar/sidebar_item.jsx
@@ -5,7 +5,7 @@ class SidebarItem extends React.Component {
     render() {
         const { photo } = this.props;
         return (
-            <div>
+            <div className='sidebar-item'>
                 <img className='sidebar-img-item' src={photo.imageURL}/>
                 <br/>
                 <div>{photo.description}</div>
