d2db9f40a068d8a216bdfc1106a9d14a9b1734b6
kxwzhang <kxwzhang@gmail.com>
2020-09-02 14:25:38 -0700

    Add dark mode to maps

--- frontend/src/components/maps/google_maps_container.jsx
+++ frontend/src/components/maps/google_maps_container.jsx
@@ -5,8 +5,91 @@ import ImageUploadFormContainer from './image_upload_form_container';
 
 const mapStyles = {
   width: "100%",
-  height: "100%",
+  height: "100%"
 };
+
+const darkMode = {
+  styles: [
+    { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+    { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+    {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#d59563' }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#d59563' }]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{ color: '#263c3f' }]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#6b9a76' }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{ color: '#38414e' }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#212a37' }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#9ca5b3' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{ color: '#746855' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#1f2835' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#f3d19c' }]
+    },
+    {
+      featureType: 'transit',
+      elementType: 'geometry',
+      stylers: [{ color: '#2f3948' }]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#d59563' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{ color: '#17263c' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#515c6d' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{ color: '#17263c' }]
+    }
+  ]  
+}
 
 export class MapContainer extends Component {
   constructor(props) {
@@ -65,7 +148,7 @@ export class MapContainer extends Component {
   render() {
     let uploadForm;
     if (this.props.location.pathname === '/upload') {
-      uploadForm = (<div>
+      uploadForm = (<div className='image-upload-form-container'>
         <ImageUploadFormContainer lat={this.state.lat} lng={this.state.lng} />
       </div>);
     }
@@ -76,6 +159,7 @@ export class MapContainer extends Component {
             className="google-api-map"
             google={this.props.google}
             zoom={12}
+            styles={darkMode.styles}
             style={mapStyles}
             gestureHandling='cooperative'
             initialCenter={{
