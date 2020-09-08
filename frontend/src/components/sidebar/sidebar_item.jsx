824fecaf5fbc30c46b422a4eb93512885b51a808
Alex Choy <alexchoy2345@gmail.com>
2020-09-07 17:46:59 -0700

    splash page css

--- frontend/src/components/sidebar/sidebar_item.jsx
+++ frontend/src/components/sidebar/sidebar_item.jsx
@@ -1,254 +1,283 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../stylesheets/sidebar.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../stylesheets/sidebar.scss";
 
 const darkMode = {
-    styles: [
-        { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-        { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-        { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-        {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }]
-        },
-        {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{ color: '#263c3f' }]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#6b9a76' }]
-        },
-        {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [{ color: '#38414e' }]
-        },
-        {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#212a37' }]
-        },
-        {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#9ca5b3' }]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [{ color: '#746855' }]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#1f2835' }]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#f3d19c' }]
-        },
-        {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{ color: '#2f3948' }]
-        },
-        {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }]
-        },
-        {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{ color: '#17263c' }]
-        },
-        {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#515c6d' }]
-        },
-        {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{ color: '#17263c' }]
-        }
-    ]
-}
+  styles: [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#263c3f" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#6b9a76" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#212a37" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#746855" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#1f2835" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#f3d19c" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#2f3948" }],
+    },
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+  ],
+};
 
 class SidebarItem extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            photo: this.props.photo,
-        }
+    this.state = {
+      photo: this.props.photo,
+    };
 
-        this.handlePanTo = this.handlePanTo.bind(this);
-        this.handleFavorites = this.handleFavorites.bind(this);
+    this.handlePanTo = this.handlePanTo.bind(this);
+    this.handleFavorites = this.handleFavorites.bind(this);
+  }
+
+  handlePanTo() {
+    const allPhotos = this.props.photos;
+    const { lat, lng } = this.props.photo.coordinates;
+    const google = window.google;
+    const mapProp = {
+      center: new google.maps.LatLng(lat, lng),
+      zoom: 12,
+    };
+    const googleAPIMap = [
+      ...document.getElementsByClassName("google-api-map"),
+    ][0];
+    const map = new google.maps.Map(googleAPIMap, mapProp);
+    // console.log("Map: ", map);
+    map.setOptions({ styles: darkMode.styles });
+    
+    // Put this in localStorage, the if statement gets triggered after 
+    // clicking on 'Location' twice
+    // map.addListener("dragend", function () {
+    //   console.log("map: ", map);
+    //   if (map.__proto__.getBounds()) {
+    //     console.log("LAT: ", map.__proto__.getBounds().getNorthEast().lat());
+    //     console.log("LNG: ", map.__proto__.getBounds().getNorthEast().lng());
+    //   }
+    // });
+    // console.log("MAP: ", map.__proto__.getBounds());
+
+    // TESTING BEGIN
+    // let newLat = null;
+    // let newLng = null;
+    // let bounds = null;
+    // while (!bounds) {
+    //     setTimeout()
+    //     bounds = map.__proto__.getBounds()
+    // }
+    // newLat = bounds.getNorthEast().lat();
+    // newLng = bounds.getNorthEast().lng();
+    // console.log(newLat)
+    // console.log(newLng)
+    // while(map.__proto__.getBounds() === undefined) {
+    //     map.__proto__.getBounds();
+    // }
+
+    // const locButton = document.getElementById("triple-click");
+    // for(let i = 0; i < 3; i++) {
+    //     return locButton.click();
+    // }
+
+    // if (map.__proto__.getBounds()) {
+    //   console.log("LAT: ", map.__proto__.getBounds().getNorthEast().lat());
+    //   console.log("LNG: ", map.__proto__.getBounds().getNorthEast().lng());
+    // }
+    // TESTING END
+
+    let markers;
+    markers = allPhotos.map((point) => {
+      const marker = new google.maps.Marker({
+        position: point.coordinates,
+        map,
+      });
+      const infowindow = new google.maps.InfoWindow({
+        content: point.description,
+      });
+      marker.addListener("click", () => {
+        infowindow.open(map, marker);
+      });
+    });
+
+    const panPoint = new google.maps.LatLng(lat, lng);
+    console.log("panPoint: ", panPoint);
+    map.panTo(panPoint);
+  }
+
+  handleFavorites() {
+    const {
+      favorites,
+      photo,
+      currentUserId,
+      makeFavorite,
+      unFavorite,
+      fetchPhotos,
+      fetchPhoto,
+    } = this.props;
+    let clickAction;
+    if (favorites[photo._id] !== undefined) {
+      if (favorites[photo._id].favoriterId === currentUserId) {
+        clickAction = unFavorite(favorites[photo._id].photoId)
+          .then((photoId) => fetchPhoto(photoId.id.data))
+          .catch((err) => console.log(err.response));
+      } else {
+      }
+    } else {
+      console.log(favorites[photo._id]);
+      console.log("hitting fave");
+      clickAction = makeFavorite({ photoId: photo._id })
+        .then((photo) => fetchPhoto(photo.favorite.data.photoId))
+        .catch((err) => console.log(err));
+    }
+    return clickAction;
+  }
+
+  render() {
+    const {
+      users,
+      currentUserId,
+      photo,
+      fetchPhotos,
+      deletePhoto,
+      makeFavorite,
+      unFavorite,
+      favorites,
+    } = this.props;
+    let deleteButton;
+    let editButton;
+    if (photo.creatorId === currentUserId) {
+      deleteButton = (
+        <i
+          className="far fa-trash-alt"
+          onClick={() => deletePhoto(photo._id).then(() => fetchPhotos())}
+        ></i>
+      );
+      editButton = (
+        <Link to={`/edit/${photo._id}`}>
+          <i className="far fa-edit"></i>
+        </Link>
+      );
     }
 
-    handlePanTo() {
-        const allPhotos = this.props.photos;
-        const { lat, lng } = this.props.photo.coordinates;
-        const google = window.google;
-        const mapProp = { 
-            center: new google.maps.LatLng(lat, lng),
-            zoom: 12
-        }
-        const googleAPIMap = [...document.getElementsByClassName('google-api-map')][0];
-        const map = new google.maps.Map(googleAPIMap, mapProp);
-        map.setOptions({ styles: darkMode.styles });
+    let favoriteButton = (
+      <div>
+        <i className="fas fa-heart" onClick={this.handleFavorites}></i>
+      </div>
+    );
 
-        console.log("MAP: ", map.__proto__.getBounds());
-        
-        let newLat = null;
-        let newLng = null;
-        let bounds = null;
-        while (!bounds) {
-            setTimeout()
-            bounds = map.__proto__.getBounds()
-        }
-        newLat = bounds.getNorthEast().lat();
-        newLng = bounds.getNorthEast().lng();
-        console.log(newLat)
-        console.log(newLng)
-        // while(map.__proto__.getBounds() === undefined) {
-        //     map.__proto__.getBounds();
-        // }
-
-        // const locButton = document.getElementById("triple-click");
-        // for(let i = 0; i < 3; i++) {
-        //     return locButton.click();
-        // }
-
-
-        if (map.__proto__.getBounds()) {
-            console.log("LAT: ", map.__proto__.getBounds().getNorthEast().lat());
-            console.log("LNG: ", map.__proto__.getBounds().getNorthEast().lng());
-        }
-
-        let markers;
-        markers = allPhotos.map(point => {
-            const marker = new google.maps.Marker({
-                position: point.coordinates,
-                map
-            });
-            const infowindow = new google.maps.InfoWindow({
-                content: point.description
-            });
-            marker.addListener("click", () => {
-                infowindow.open(map, marker);
-            });
-        })
-
-        const panPoint = new google.maps.LatLng(lat, lng);
-        map.panTo(panPoint);
+    if (
+      favorites[photo._id] &&
+      favorites[photo._id].favoriterId === currentUserId
+    ) {
+      favoriteButton = (
+        <div className="favorite-like">
+          <i className="fas fa-heart" onClick={this.handleFavorites}></i>
+        </div>
+      );
     }
 
-    handleFavorites() {
-        const { 
-            favorites, 
-            photo, 
-            currentUserId, 
-            makeFavorite, 
-            unFavorite, 
-            fetchPhotos,
-            fetchPhoto
-        } = this.props;
-        let clickAction;
-        if (favorites[photo._id] !== undefined) {
-            if (favorites[photo._id].favoriterId === currentUserId) {
-                clickAction = unFavorite(favorites[photo._id].photoId)
-                    .then(photoId => fetchPhoto(photoId.id.data))
-                    .catch(err => console.log(err.response));
-            } else {
-            }
-        } else {
-            console.log(favorites[photo._id]);
-            console.log('hitting fave');
-            clickAction = makeFavorite({ photoId: photo._id })
-                .then(photo => fetchPhoto(photo.favorite.data.photoId)).catch(err => console.log(err));
-        } 
-        return clickAction;
-    }
-
-    render() {
-        const { 
-            users,
-            currentUserId, 
-            photo, 
-            fetchPhotos, 
-            deletePhoto, 
-            makeFavorite, 
-            unFavorite,
-            favorites 
-        } = this.props;
-        let deleteButton;
-        let editButton;
-        if (photo.creatorId === currentUserId) {
-            deleteButton = (
-                <i className="far fa-trash-alt" onClick={() => deletePhoto(photo._id).then(() => fetchPhotos())}></i>
-            );
-            editButton = (
-                <Link to={`/edit/${photo._id}`}>
-                    <i className="far fa-edit"></i>
-                </Link>
-            );
-        }
-
-        let favoriteButton = (
-            <div>
-                <i className="fas fa-heart" onClick={this.handleFavorites}></i>
+    if (!users[photo.creatorId]) {
+      return null;
+    } else {
+      return (
+        <div className="sidebar-item">
+          <img className="sidebar-img-item" src={photo.imageURL} />
+          <br />
+          <div className="sidebar-item-description">"{photo.description}"</div>
+          <br />
+          <div className="sidebar-item-other-info">
+            <div className="sidebar-username">
+              @{users[photo.creatorId].username}
             </div>
-        )
-
-        if(favorites[photo._id] && (favorites[photo._id].favoriterId === currentUserId)) {
-            favoriteButton = (
-                <div className="favorite-like">
-                    <i className="fas fa-heart" onClick={this.handleFavorites}></i>
-                </div>
-            );
-        }
-
-        if (!users[photo.creatorId]) {
-            return null;
-        } else {
-            return (
-                <div className='sidebar-item'>
-                    <img className='sidebar-img-item' src={photo.imageURL}/>
-                    <br/>
-                    <div className="sidebar-item-description">"{photo.description}"</div>
-                    <br/>
-                    <div className="sidebar-item-other-info">
-                        <div className='sidebar-username'>@{users[photo.creatorId].username}</div>
-                        <div className="sidebar-item-timestamp">{photo.created}</div>
-                        <div className='sidebar-item-tags'>Tags: {photo.tags.join(', ')}</div>
-                    </div>
-                    <br/>
-                    <div className="sidebar-item-actions">
-                        <div className="item-favorites">
-                            {favoriteButton}
-                            {/* <i className="fas fa-heart" onClick={this.handleFavorites}></i> */}
-                            <div className="numFavorites">{photo.numFavorites}</div>
-                        </div>
-                        {deleteButton}
-                        {editButton}
-                        <i id="triple-click" className="fas fa-map-marker-alt" onClick={this.handlePanTo}></i>
-                    </div>
-                </div>
-            );
-        }     
+            <div className="sidebar-item-timestamp">{photo.created}</div>
+            <div className="sidebar-item-tags">
+              Tags: {photo.tags.join(", ")}
+            </div>
+          </div>
+          <br />
+          <div className="sidebar-item-actions">
+            <div className="item-favorites">
+              {favoriteButton}
+              {/* <i className="fas fa-heart" onClick={this.handleFavorites}></i> */}
+              <div className="numFavorites">{photo.numFavorites}</div>
+            </div>
+            {deleteButton}
+            {editButton}
+            <i
+              id="triple-click"
+              className="fas fa-map-marker-alt"
+              onClick={this.handlePanTo}
+            ></i>
+          </div>
+        </div>
+      );
     }
+  }
 }
 
-export default SidebarItem; 
+export default SidebarItem;
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
 class Splash extends React.Component {
-    componentDidMount() {
-        window.scrollTo(0, 0);
-    }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-    render() {
-        return (
-            <div className='splash-container'>
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="splash-container">
+        <h2 className="splash-header">See the world through your own window.</h2>
+        <img src="../../images/homepage copy.png" className='splash-screenshot' />
+        {/* <h2>The Image Geo-Tagging Social Media Platform that was developed by Kev Win Ad and Al</h2> */}
+      </div>
+    );
+  }
 }
 
 export default Splash;
